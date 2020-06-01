ecb3306bc0d6ceb58cde6346815ff2eb7ab1386c
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-01 13:46:10 -0700

    add division function to html.

--- Desktop/calculator/js/scripts.js
+++ Desktop/calculator/js/scripts.js
@@ -1,10 +1,10 @@
-const subtract = function(number1, number2) {
-  return number1 - number2;
+const divide = function(number1, number2) {
+  return number1/number2;
 };
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-const result = subtract(number1, number2);
+const result = divide(number1, number2);
 
 alert(result);
